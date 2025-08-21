d74b21d0e82c35ad9e4cebbdc83949af1c811fd0
nbt2477 <neeraj.s@paynearby.in>
2025-08-22 03:19:49 +0530

    Adding unit test stage

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/')));
 app.use(cors())
 
+mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGO_URI, {
     user: process.env.MONGO_USERNAME,
     pass: process.env.MONGO_PASSWORD,
